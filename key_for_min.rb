9575cfd60ed9d45c1f273499516b0545de5405f3
Alla Kuzminova <alchik@gmail.com>
2020-09-21 23:54:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  def smallest_value
-  def return_key
+  smallest_value - nil
+  return_key = nil
   if(name_hash.size() == 0)
     return nil
   end
